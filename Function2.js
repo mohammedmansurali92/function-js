e80e8fb5be79767af869bc657b59c9fc0ea144f6
Mohammad Mansur Ali <80177222+mohammedmansurali92@users.noreply.github.com>
2023-08-19 09:12:44 -0700

    function parameter
    This file include function double parameter.

---
+++ Function2.js
@@ -0,0 +1,13 @@
+//creating a function
+
+function square(num1, num2) {
+
+    var result = num1 * num2;
+    document.write("Result=" + result + "<br/>");
+
+
+}
+//call a function
+square(5, 6);
+square(7, 8);
+square(9, 4);
