5a1e7462149c46a69d48c0844c42a9162d61c388
Mohammad Mansur Ali <80177222+mohammedmansurali92@users.noreply.github.com>
2023-08-19 09:07:12 -0700

    function1 js
    This file included a function single parameter.

---
+++ function1.js
@@ -0,0 +1,13 @@
+//creating a function
+
+function square(num) {
+
+    var result = num * num;
+    document.write("Result=" + result + "<br/>");
+
+
+}
+//call a function
+square(5);
+square(6);
+square(7);
