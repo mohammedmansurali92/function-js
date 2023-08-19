64b3b84ce659cde6fe773462faae967cf1ddbbb1
Mohammad Mansur Ali <80177222+mohammedmansurali92@users.noreply.github.com>
2023-08-19 09:02:17 -0700

    Function JS
    This file includes function codding.

---
+++ function.js
@@ -0,0 +1,13 @@
+//creating a function
+
+function square(num) {
+    num = 5;
+    var result = num * num;
+    document.write("Result=" + result + "<br/>");
+
+
+}
+//call a function
+square();
+square();
+square();
